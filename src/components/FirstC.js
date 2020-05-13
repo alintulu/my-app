3c39e3ebfcb7eaae41af674818fc092bd230e974
Adelina Lintuluoto <adelina.eleonora.lintuluoto@cern.ch>
2020-05-13 13:33:37 +0100

    add test

---
+++ src/components/FirstC.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FirstC extends React {
+    render() {
+      return  <div>
+                <h2>First Component</h2>
+                <h3>On Your Way to One Of Many Great Scenes.</h3>
+              </div>;
+    }
+  }
+  
+export default FirstC;
